f5866526030061143b054470892c64f704caa11f
zhufucdev <zhufuzhufu1@gmail.com>
2025-02-11 22:31:01 +0800

    :bug: wrong multiline alignment

--- iosApp/iosApp/Composed/TextFrameView.swift
+++ iosApp/iosApp/Composed/TextFrameView.swift
@@ -6,6 +6,7 @@ struct TextFrameView : View {
     var frame: TextFrame
     var body: some View {
         Text(frame.content)
+            .multilineTextAlignment(.leading)
             .frame(maxWidth: .infinity, alignment: .leading)
     }
 }
