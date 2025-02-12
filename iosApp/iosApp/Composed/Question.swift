6bdec46134e1add0c5adfc638012c9a1a9948d13
zhufucdev <zhufuzhufu1@gmail.com>
2025-02-12 09:34:38 +0800

    :art: add preview to question (the view)

--- iosApp/iosApp/Composed/Question.swift
+++ iosApp/iosApp/Composed/Question.swift
@@ -35,3 +35,15 @@ struct Question : View {
         .padding()
     }
 }
+
+#Preview {
+    Question(data: [
+        PrioritizedFrame(frame: FrameText(id: 0, textFrame: TextFrame(id: 0, embeddingsId: nil, content: "What's the meaning of life?")), priority: 0),
+        PrioritizedFrame(frame: FrameOptions(optionsFrame: OptionsFrame(id: 1, name: "Choose several of the following 4"), frames: [
+            KeyedPrioritizedFrame(frame: FrameText(id: 1, textFrame: TextFrame(id: 1, embeddingsId: nil, content: "To have fun")), isKey: true, priority: 0),
+            KeyedPrioritizedFrame(frame: FrameText(id: 2, textFrame: TextFrame(id: 2, embeddingsId: nil, content: "To find one's meaning")), isKey: true, priority: 1),
+            KeyedPrioritizedFrame(frame: FrameText(id: 3, textFrame: TextFrame(id: 3, embeddingsId: nil, content: "To help others")), isKey: true, priority: 3),
+            KeyedPrioritizedFrame(frame: FrameText(id: 4, textFrame: TextFrame(id: 4, embeddingsId: nil, content: "To be remembered")), isKey: true, priority: 4),
+        ]), priority: 1)
+    ])
+}
